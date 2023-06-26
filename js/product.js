c46156d587e6265819397f42b3e4704e8743c43d
Tezal Thapa <137768102+tezalthapa10@users.noreply.github.com>
2023-06-26 14:54:30 +0545

    Initialized

---
+++ js/product.js
@@ -0,0 +1,9 @@
+//pop up feature for buy now and add to cart button
+function buyNow() {
+    alert("Thank you for purchasing! Your order will be processed shortly.");
+  }
+  
+  function addToCart() {
+    alert("The item has been added to your cart.");
+}
+
